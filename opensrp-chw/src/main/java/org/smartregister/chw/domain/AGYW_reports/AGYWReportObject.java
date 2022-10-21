e0d2fe7ded15de77d25e03308b8c9afdfad1eee5
tbwahacker <twaloadam@gmail.com>
2022-10-21 11:20:27 +0300

    added html files for agyw inapp reports

--- opensrp-chw/src/main/java/org/smartregister/chw/domain/AGYW_reports/AGYWReportObject.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/domain/AGYW_reports/AGYWReportObject.java
@@ -10,19 +10,25 @@ import java.util.Date;
 public class AGYWReportObject extends ReportObject {
 
 
+    private final String[] noOfQuestions = new String[]
+            {"1","2","3","4","5","6","7","8","9","10","11","12","13"};
     private final String[] questionsGroups = new String[]{
-            "2","2-i","2-ii","2-iii","2-iv","2-v","2-vi",
-            "3","3-i","3-ii","3-iii","3-iv","3-v","3-vi",
-            "7","7-i","7-ii","7-iii","7-iv","7-v","7-vi",
-            "8","8-i","8-ii","8-iii","8-iv","8-v","8-vi",
-            "9","9-i","9-ii","9-iii","9-iv","9-v","9-vi"
+            "1-10-14","1-15-19","1-20-24",
+            "2-10-14","2-15-19","2-20-24",
+            "3-10-14","3-15-19","3-20-24",
+            "4-10-14","4-15-19","4-20-24",
+            "5-10-14","5-15-19","5-20-24",
+            "6-10-14","6-15-19","6-20-24",
+            "7-10-14","7-15-19","7-20-24",
+            "8-10-14","8-15-19","8-20-24",
+            "9-10-14","9-15-19","9-20-24",
+            "10-10-14","10-15-19","10-20-24",
+            "11-10-14","11-15-19","11-20-24",
+            "12-10-14","12-15-19","12-20-24",
+            "13-10-14","13-15-19","13-20-24",
     };
-    private final String[] ageGroups = new String[]{
-            "15-19","20-24","25-29","30-34","35-39","40-44","45-49",">50"
-    };
-    private final String[] genderGroups = new String[]{
-            "ME","KE"
-    };
+
+
 
     private final Date reportDate;
     private JSONObject jsonObject ;
@@ -35,39 +41,23 @@ public class AGYWReportObject extends ReportObject {
     @Override
     public JSONObject getIndicatorData() throws JSONException {
         jsonObject = new JSONObject();
-        for (String questionGroup : questionsGroups) {   //rows
-            for (String ageGroup : ageGroups) {  //columns
-                for (String genderGroup : genderGroups) {  //concstenate rows columns and gendergroup
-                        jsonObject.put("hivst"+"-"+questionGroup+"-"+ageGroup+"-"+genderGroup,
-                                ReportDao.getReportPerIndicatorCode("hivst"+"-"+questionGroup+"-"+ageGroup+"-"+genderGroup, reportDate));
-                }
-            }
+        for (String questionGroup : questionsGroups) {
+                        jsonObject.put("agyw"+"-"+questionGroup,
+                                ReportDao.getReportPerIndicatorCode("agyw"+"-"+questionGroup, reportDate));
         }
-        // get total of all Male & Female in Qn 2 & 7
-        //and the whole total for both of them
-        funcGetTotal();
+        // get total of all
+        getTotalPerEachIndicator();
 
         return jsonObject;
     }
 
-    private int getTotalPerEachIndicator(String total_indicatorCode, String question, String gender) throws JSONException {
-        int totalOfGenderGiven = 0;
-        for (String agegroup: ageGroups){
-                totalOfGenderGiven += ReportDao.getReportPerIndicatorCode(total_indicatorCode+"-"+agegroup+"-"+gender, reportDate);
-            jsonObject.put("hivst"+"-"+question+"-jumla-"+gender,totalOfGenderGiven);  //display the total for specified gender
+    private int getTotalPerEachIndicator() throws JSONException {
+        int finalTotal = 0;
+        for (String question: noOfQuestions){
+                finalTotal += ReportDao.getReportPerIndicatorCode("agyw"+"-"+question+"-JUMLA", reportDate);
+            jsonObject.put("agyw"+"-"+question+"-JUMLA",finalTotal);  //display the total for specified gender
         }
-        return totalOfGenderGiven;
-    }
-
-
-
-    private void funcGetTotal() throws JSONException {
-        for (String question: questionsGroups) {   //rows
-            int totalOfBothMaleAndFemale = getTotalPerEachIndicator("hivst"+"-"+question,question,"ME")
-                            + getTotalPerEachIndicator("hivst"+"-"+question,question,"KE");
-            jsonObject.put("hivst"+"-"+question+"-jumla-both-ME",totalOfBothMaleAndFemale);
-            jsonObject.put("hivst"+"-"+question+"-jumla-both-KE",totalOfBothMaleAndFemale);
-        }
+        return finalTotal;
     }
 
 
